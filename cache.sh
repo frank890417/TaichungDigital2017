7179d84a5f7e5628a682b304b89bdc0087533a4b
cheyuwu <frank890417@gmail.com>
2017-05-02 11:56:00 +0800

    update indep work

---
+++ cache.sh
@@ -0,0 +1,14 @@
+php artisan config:clear
+php artisan route:clear
+php artisan clear-compiled
+php artisan cache:clear
+
+chmod -R 777 app/storage
+
+composer dump-autoload
+
+php artisan config:cache
+php artisan route:cache
+php artisan optimize --force
+gulp --production
+
